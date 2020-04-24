6b4bc4f25d8b4206a5b3aeebe6686144694d3a3f
604karev <604karev@gmail.com>
2020-04-24 13:30:55 +0300

    added authentication component, created useState and useEffects hooks

---
+++ src/pages/authentication/index.js
@@ -0,0 +1,85 @@
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+import axios from 'axios'
+
+const Authentication = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsSubmitted(true)
+    };
+
+    useEffect(
+        () => {
+            if (!isSubmitted) {
+                return
+            }
+            axios(
+                'https://conduit.productionready.io/api/users/login',
+                {
+                    method: 'post',
+                    data: {
+                        user: {
+                            email: 'ematil@mail.com',
+                            password: '1234'
+                        }
+                    }
+                }
+            )
+                .then(res => {
+                        console.log(`dsadsda`,res);
+                        setIsSubmitted(false)
+                    }
+                )
+                .catch(error => {
+                    console.log(`some`,error);
+                    setIsSubmitted(false)
+                })
+        }
+    );
+
+    return (
+        <div className="auth-page">
+            <div className="container page">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 col-xs-12">
+                        <h1 className="text-xs-center">Login</h1>
+                        <p className="text-xs-center">
+                            <Link to={`/register`}>Need an account?</Link>
+                        </p>
+                        <form onSubmit={handleSubmit}>
+                            <fieldset>
+                                <fieldset className='form-group'>
+                                    <input
+                                        type="email"
+                                        className='form-control form-control-lg'
+                                        placeholder='Email'
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </fieldset>
+                                <fieldset className='form-group'>
+                                    <input
+                                        type="password"
+                                        className='form-control form-control-lg'
+                                        placeholder='Password'
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}/>
+                                </fieldset>
+                                <button className="btn btn-lg btn-primary pull-xs-right" type='submit'
+                                        disabled={isSubmitted}>Sign in
+                                </button>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+};
+export default Authentication
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -2,10 +2,13 @@ import React from "react";
 import {Switch, Route} from 'react-router-dom'
 import GlobalFeed from "pages/globalFeed";
 import Article from "pages/article";
+import Authentication from 'pages/authentication'
 
 export default () => (
     <Switch>
         <Route path='/' component={GlobalFeed} exact />
+        <Route path='/login' component={Authentication} />
+        <Route path='/register' component={Authentication} exact />
         <Route path='/article/:slug' component={Article}/>
     </Switch>
 )
